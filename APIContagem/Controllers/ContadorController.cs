37adbba8413ea13137563839a04612ac1ebb7947
Renato Groffe <renato.groffe@yahoo.com.br>
2019-09-03 13:23:29 -0300

    ASP.NET Core 2.2 + .NET Core 2.2 + Docker + Docker Compose + Environment Variables
    ASP.NET Core 2.2 + .NET Core 2.2 + Docker + Docker Compose + Environment Variables

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APIContagem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get([FromServices]IConfiguration configuration)
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString,
+                    Variavel =  configuration["TesteAmbiente"], // Environment.GetEnvironmentVariable("TesteAmbiente"),
+                    TargetFramework = Assembly
+                        .GetEntryAssembly()?
+                        .GetCustomAttribute<TargetFrameworkAttribute>()?
+                        .FrameworkName
+                };
+            }
+        }
+    }
+}
